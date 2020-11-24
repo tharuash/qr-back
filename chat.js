b4d31a9cab8764f705c0459c217057d23b000d9c
tharuash <tharinduash@outlook.com>
2020-11-24 09:20:28 +0530

    chat added

--- chat.js
+++ chat.js
@@ -7,14 +7,11 @@ io.on('connection', (socket) => {
 
 var onlineUsers = []
 io.on('connection', async function (socket) {
-    // attach incoming listener for new user
+   
     socket.on("user_connected", function (userId) {
-        // save in array
+        
         users[userId] = socket.id;
-
-        // socket ID will be used to send message to individual person
-
-        // notify all connected clients
+  
         io.emit("user_connected", userId);
     });
 })
